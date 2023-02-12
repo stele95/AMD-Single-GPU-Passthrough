f97c15896ca50babf0d6f4fd09bd7870c0afdbfa
Stefan Dimitrijevic <stefanstele95@hotmail.com>
2023-02-12 14:54:13 +0100

    change how setting up cpu governor hooks works

--- setup_cpu_governor_hooks.sh
+++ setup_cpu_governor_hooks.sh
@@ -7,4 +7,5 @@ echo "This will configure CPU governor hooks."
 cp -r -i hooks /etc/libvirt
 chmod +x /etc/libvirt/hooks/*/prepare/begin/cpu_mode_performance.sh
 chmod +x /etc/libvirt/hooks/*/release/end/cpu_mode_*.sh
+cat qemu_cpu_governor_hooks >> /etc/libvirt/hooks/qemu
 echo "CPU governor hooks set up successfully!"
